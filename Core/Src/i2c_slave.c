5ed0e48676efec73c7341d7ef99f129b84a00159
Johannes Matejcek <johannesmatejcek@web.de>
2024-07-09 18:31:33 +0200

    weiter gemacht am slave.

--- Core/Src/i2c_slave.c
+++ Core/Src/i2c_slave.c
@@ -13,16 +13,20 @@
 uint8_t dummy_in;
 RINGBUFFER_DataItem data;
 
-//void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode){
-//	if (TransferDirection == I2C_DIRECTION_TRANSMIT)  // if the master wants to transmit the data
-//		{
-//			//HAL_I2C_Slave_Seq_Receive_IT(hi2c, &dummy_in, 1, I2C_FIRST_FRAME);
-//			RINGBUFFER_reset();
-//		}
-//	else{
-//		RINGBUFFER_dequeue(&data);
-//
-//		//TODO: put into dataOutArr
-//		HAL_I2C_Slave_Seq_Transmit_IT(hi2c, (uint8_t*)&data, 9, 0);
-//	}
-//}
+void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c){
+	HAL_I2C_EnableListen_IT(hi2c2); // Start listening again
+}
+
+void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode){
+	if (TransferDirection == I2C_DIRECTION_TRANSMIT)  // if the master wants to transmit the data
+		{
+			//HAL_I2C_Slave_Seq_Receive_IT(hi2c, &dummy_in, 1, I2C_FIRST_FRAME);
+			RINGBUFFER_reset();
+		}
+	else{
+		RINGBUFFER_dequeue(&data);
+
+		//TODO: put into dataOutArr
+		HAL_I2C_Slave_Seq_Transmit_IT(hi2c, (uint8_t*)&data, 9, 0);
+	}
+}
